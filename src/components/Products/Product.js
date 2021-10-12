766f1632805efed3c25c2a6633c7637137f632fe
skyblue813 <pthoppae@emich.edu>
2021-10-12 19:09:24 +0000

    added the image and description in the product.js

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,28 +1,32 @@
 import { useParams } from "react-router";
 import {commerce} from "../../lib/commerce";
-import {useEffect,useState} from "react";
-
+import {useEffect,useState} from "react"
 
 
 function Product(){
     let {ProductId}=useParams();
-    const[product,setProduct]=useState([]);
+    const[product,setProduct]=useState(undefined);
     useEffect(()=>{
         console.log(ProductId);
         commerce.products.retrieve(ProductId).then(response=>
             {setProduct(response);
                 });
             },[ProductId]);
-    return(
-    <div>
-        <p> My Product: {product.name}</p> 
-        <p> {product.description}</p>
 
-
-
-    </div>
-
-    );      
+            return(
+                <main>
+                    {product !== undefined && 
+                    <>
+                    <img width = {250} 
+                        key = {product.id} 
+                        src = {product.image.url} 
+                        alt= {product.name} />
+                    <h2>{product.name}</h2>
+                    <h3>${product.price.formatted}</h3>
+                    <div dangerouslySetInnerHTML={{ __html: product.description}} />
+                    </>}
+                </main>
+            );  
         
 }
 
